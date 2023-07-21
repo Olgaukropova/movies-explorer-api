1d089c5815c726eef843a8ff69b512a66bd5ab62
Olga Uk <olgablackangel@bk.ru>
2023-07-22 00:02:49 +0300

    start

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ const authRoutes = require('./login');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('/api', authRoutes);
-router.use('/api/users', auth, userRoutes);
-router.use('/api/movies', auth, movieRoutes);
+router.use('/', authRoutes);
+router.use('/users', auth, userRoutes);
+router.use('/movies', auth, movieRoutes);
 
 router.use('*', (req, res, next) => next(new NotFoundError('Указанный путь не найден.')));
 
